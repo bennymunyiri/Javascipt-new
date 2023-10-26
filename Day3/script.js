9f021c6dd3d8e25a74e1266c983d4ecb5b578959
bennymunyiri <munyiriben00@gmail.com>
2023-10-26 15:01:31 +0300

    Great and cool

---
+++ Day3/script.js
@@ -0,0 +1,53 @@
+// switch case
+
+// let statusVal = 2;
+
+// switch (statusVal) {
+//     case 1:
+//         {
+//             console.log("Super User");
+//             break;
+//         }
+//     case 2:
+//         {
+//             console.log("Medium User");
+//             break;
+//         }
+//     case 3:
+//         {
+//             console.log("User");
+//             break;
+//         }
+
+
+//     default:
+//         {
+//             console.log("Lower Level User");
+//             break;
+//         }
+// }
+
+// if (statusVal === 1) {
+//     console.log("super user");
+// }
+
+// else if (statusVal === 2) {
+//     console.log("Medium User");
+// }
+
+// else {
+//     console.log("Lower level");
+// }
+
+// let age = 17;
+
+// // ternary operator
+// // let votestatus = age >= 18 ? 1 : 2;
+
+// console.log(votestatus)
+
+let student1 = {
+    name: "Benson",
+    age: 27,
+    city: "Mumbai"
+}
