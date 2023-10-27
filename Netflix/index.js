dd71d36948628b01e8d306f291515fc9e6494ef9
bennymunyiri <munyiriben00@gmail.com>
2023-10-27 11:15:58 +0300

    hello

---
+++ Netflix/index.js
@@ -0,0 +1,19 @@
+// let movies = [
+//     {
+//         name: "Avengers Infinity War",
+//         rating: 8.7,
+//         poster: "https://m.media-amazon.com/images/I/91zzAMkVCUL._AC_UF894,1000_QL80_.jpg",
+//         description: "They feel that by engaging in terrorism they become the avengers of a great wrong done to their people."
+//     }
+
+// ]
+
+// let movie = {
+//     name: "Avengers Infinity War",
+//     rating: 8.7,
+//     poster: "https://m.media-amazon.com/images/I/91zzAMkVCUL._AC_UF894,1000_QL80_.jpg",
+//     description: "They feel that by engaging in terrorism they become the avengers of a great wrong done to their people."
+// }
+
+
+// functions
